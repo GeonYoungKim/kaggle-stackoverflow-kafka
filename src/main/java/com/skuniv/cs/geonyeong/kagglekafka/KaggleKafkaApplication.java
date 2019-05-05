f13721e0711d9ef10a6f5ad6f3fb85fc3afdb3dd
GEONYOUNGKIM <c2619zz@naver.com>
2019-05-06 02:01:37 +0900

    kaggle stackoverflow diff data kafka consume project init commit

---
+++ src/main/java/com/skuniv/cs/geonyeong/kagglekafka/KaggleKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.skuniv.cs.geonyeong.kagglekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaggleKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaggleKafkaApplication.class, args);
+    }
+
+}
